ed546995becf3616dc9fa63d5fe5968ca6cf01d8
uTatsii <utatsiy@gmail.com>
2022-08-27 20:50:44 +0300

    completed rendering and deligation

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const galleryMarkup = galleryItems
+  .map(
+    (galleryItem) => `<div class='gallery__item'>
+<img class='gallery__image' src='${galleryItem.preview}' 
+alt='${galleryItem.description}'/></div>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", galleryMarkup);
+
+gallery.addEventListener('click', onImgClick);
+
+function onImgClick(e) {
+    if (e.target.classList === 'gallery') {
+        return;
+    }
+
+    const selectedImg = e.target;
+    const neededIndex = galleryItems.findIndex(item => item.description === selectedImg.alt);
+    const newLink = galleryItems[neededIndex].original;
+    selectedImg.src = newLink;
+    console.log(selectedImg.src);
+}
+console.log(gallery.classList);
